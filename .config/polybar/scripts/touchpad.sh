12a1c93a8c57cc49c484f0718426756fdb045b67
Kevin Carrion <86639616+kevinzl13@users.noreply.github.com>
2026-08-13 19:37:50 -0500

    se añadio buton para activar y desactivar touchpad

---
+++ .config/polybar/scripts/touchpad.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+TOUCHPAD_ID="11"
+
+get_status() {
+    xinput list-props "$TOUCHPAD_ID" |
+        awk '/Device Enabled/{print $NF}'
+}
+
+case "$1" in
+    toggle)
+        if [ "$(get_status)" = "1" ]; then
+            xinput disable "$TOUCHPAD_ID"
+        else
+            xinput enable "$TOUCHPAD_ID"
+        fi
+        ;;
+
+    status)
+        if [ "$(get_status)" = "1" ]; then
+            echo "󰟸 ON"
+        else
+            echo "󰤳 OFF"
+        fi
+        ;;
+esac
